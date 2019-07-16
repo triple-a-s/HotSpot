dd1acbe7d701ba1f3dd6b34c5692e164d9505e28
Andrea Lin <drealin@fb.com>
2019-07-16 15:46:23 -0700

    Booking subclasses PFObject

---
+++ HotSpot/Models/Booking.h
@@ -0,0 +1,17 @@
+//
+//  Booking.h
+//  HotSpot
+//
+//  Created by drealin on 7/16/19.
+//  Copyright © 2019 aodemuyi. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Booking : PFObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HotSpot/Models/Booking.m
@@ -0,0 +1,17 @@
+//
+//  Booking.m
+//  HotSpot
+//
+//  Created by drealin on 7/16/19.
+//  Copyright © 2019 aodemuyi. All rights reserved.
+//
+
+#import "Booking.h"
+@interface Booking()<PFSubclassing>
+@end
+
+@implementation Booking
++ (nonnull NSString *)parseClassName {
+    return @"Booking";
+}
+@end
