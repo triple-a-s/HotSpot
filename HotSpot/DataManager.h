d8d083a611a4cf7bbdaeb5c14effb944aa7a78a3
Andrea Lin <drealin@fb.com>
2019-07-16 15:29:22 -0700

    create DataManager class

---
+++ HotSpot/DataManager.h
@@ -0,0 +1,17 @@
+//
+//  DataManager.h
+//  HotSpot
+//
+//  Created by drealin on 7/16/19.
+//  Copyright © 2019 aodemuyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DataManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HotSpot/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  HotSpot
+//
+//  Created by drealin on 7/16/19.
+//  Copyright © 2019 aodemuyi. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end
