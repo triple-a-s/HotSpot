a4105a7252960109a0477117dcdf786fbd1b4782
Andrea Lin <drealin@fb.com>
2019-07-18 14:43:29 -0700

    create blank view controllers

---
+++ HotSpot/View Controllers/BookingViewController.h
@@ -0,0 +1,17 @@
+//
+//  BookingViewController.h
+//  HotSpot
+//
+//  Created by drealin on 7/18/19.
+//  Copyright © 2019 aodemuyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BookingViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HotSpot/View Controllers/BookingViewController.m
@@ -0,0 +1,32 @@
+//
+//  BookingViewController.m
+//  HotSpot
+//
+//  Created by drealin on 7/18/19.
+//  Copyright © 2019 aodemuyi. All rights reserved.
+//
+
+#import "BookingViewController.h"
+
+@interface BookingViewController ()
+
+@end
+
+@implementation BookingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
