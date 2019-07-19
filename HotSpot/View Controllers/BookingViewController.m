f2aa675c03a3db22405758ca06e32ccbbecfb2e4
Andrea Lin <drealin@fb.com>
2019-07-19 10:15:05 -0700

    book to Parse database

--- HotSpot/View Controllers/BookingViewController.m
+++ HotSpot/View Controllers/BookingViewController.m
@@ -7,15 +7,20 @@
 //
 
 #import "BookingViewController.h"
+#import "Booking.h"
 
 @interface BookingViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *listingImageView;
 @property (weak, nonatomic) IBOutlet UILabel *listingAddressLabel;
 @property (weak, nonatomic) IBOutlet UILabel *listingPriceLabel;
 @property (weak, nonatomic) IBOutlet UILabel *listingOwnerLabel;
+@property (weak, nonatomic) IBOutlet UIDatePicker *startDatePicker;
+@property (weak, nonatomic) IBOutlet UIDatePicker *endDatePicker;
 @end
 
 @implementation BookingViewController
+
+# pragma mark - Private Methods
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -34,6 +39,19 @@
 - (IBAction)closeClicked:(id)sender {
     [self dismissViewControllerAnimated:true
                              completion:nil];
+}
+
+- (IBAction)confirmClicked:(id)sender {
+    [Booking bookDriveway:self.listing
+            withStartTime:_startDatePicker.date
+        withDurationInSec:[[NSNumber alloc] initWithDouble:[self.endDatePicker.date timeIntervalSinceDate:self.startDatePicker.date]] withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
+            if(succeeded) {
+                [self performSegueWithIdentifier:@"confirmationSegue" sender:nil];
+            }
+            else {
+                NSLog(@"%@", error);
+            }
+        }];
 }
 
 @end
