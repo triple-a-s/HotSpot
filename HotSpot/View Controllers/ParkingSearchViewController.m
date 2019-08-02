42caa19b6c8afd8ee5e4dfce8fa6eec557728800
Ayooluwa Odemuyiwa <aodemuyi@fb.com>
2019-08-02 16:43:13 -0700

    tableView working

--- HotSpot/View Controllers/ParkingSearchViewController.m
+++ HotSpot/View Controllers/ParkingSearchViewController.m
@@ -29,6 +29,8 @@
     self.searchTableView.rowHeight = 134;
     self.searchTableView.delegate = self;
     self.searchTableView.dataSource = self;
+    
+    // will replace this with user location
     self.initialLocation = [[CLLocation alloc] initWithLatitude:37.44 longitude:-122.344];
     PFGeoPoint *geoPoint = [PFGeoPoint geoPointWithLatitude:self.initialLocation.coordinate.latitude longitude:self.initialLocation.coordinate.longitude]; // san francisco
     [DataManager getListingsNearLocation:geoPoint withCompletion:^(NSArray<Listing *> * _Nonnull listings, NSError * _Nonnull error) {
@@ -37,6 +39,7 @@
         }
         else{
             self.listings = listings;
+            // in case the update is done on the wrong thread
             dispatch_async(dispatch_get_main_queue(), ^{
             [self.searchTableView reloadData];
             });
@@ -50,6 +53,18 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
+    if (self.listings.count == 0){
+        SearchCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SearchCell"];
+        if(cell == nil){
+            cell = [[SearchCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"SearchCell"];
+        }
+        cell.searchTableImage.image = [UIImage imageNamed:@"ourlogo"];
+        cell.searchTablePrice.text = @"No Spots";
+        cell.searchTableAddress.text = @" ";
+        cell.searchTableMilesAway.text = @" please try a different location";
+        return cell;
+    }
+    else{
     SearchCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SearchCell"];
     if(cell == nil){
         cell = [[SearchCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"SearchCell"];
@@ -76,9 +91,14 @@
     // trying to resize text to work with Autolayout
     cell.searchTablePrice.adjustsFontSizeToFitWidth = YES;
     return cell;
+    }
+    
 }
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if(self.listings.count == 0){
+        return 1;
+    }
     return self.listings.count;
 }
 
