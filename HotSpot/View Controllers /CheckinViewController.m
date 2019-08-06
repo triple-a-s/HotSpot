4151b48e7c3df24fca0dfed2d2090211a082e3ff
Andrea Lin <drealin@fb.com>
2019-08-05 18:19:40 -0700

    details for the booking and autolayout

--- HotSpot/View Controllers /CheckinViewController.m
+++ HotSpot/View Controllers /CheckinViewController.m
@@ -10,9 +10,14 @@
 
 #import "Booking.h"
 #import "CancelViewController.h"
+#import "DataManager.h"
 
 @interface CheckinViewController ()
 @property (strong, nonatomic) Booking *booking;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalPriceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *startTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *endTimeLabel;
 @end
 
 @implementation CheckinViewController
@@ -34,6 +39,19 @@
         }
         else {
             self.booking = object;
+            Booking *booking = object;
+            [booking.listing fetchInBackgroundWithBlock:^(PFObject * _Nullable object, NSError * _Nullable error) {
+                Listing *listing = object;
+                [DataManager getAddressNameFromPoint:listing.address withCompletion:^(NSString *name, NSError * _Nullable error){
+                    if(error) {
+                        NSLog(@"%@", error);
+                    }
+                    else {
+                        self.addressLabel.text = name;
+                    }
+                }];
+            }];
+            self.startTimeLabel.text = [NSString stringWithFormat:@"Start time: %@", booking.startTime];
         }
     }];
 }
