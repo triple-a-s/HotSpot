aed740d011a79d9614db825eb9f6b01cdbc498ad
Ayooluwa Odemuyiwa <aodemuyi@fb.com>
2019-08-08 16:57:11 -0700

    profile confirmation animation

--- HotSpot/View Controllers/TransitionViewController.m
+++ HotSpot/View Controllers/TransitionViewController.m
@@ -8,15 +8,30 @@
 
 #import "TransitionViewController.h"
 #import <FLAnimatedImage/FLAnimatedImage.h>
+#import "FancyButton.h"
 
 @interface TransitionViewController ()
 @property (weak, nonatomic) IBOutlet FLAnimatedImageView *carImage;
+@property (weak, nonatomic) IBOutlet FancyButton *profileButton;
 
 @end
 
 @implementation TransitionViewController
 
 - (void)viewDidLoad {
+    [UIView animateWithDuration: 2 delay:0.0f options:UIViewAnimationOptionRepeat
+                     animations:^{
+                         self.profileButton.transform = CGAffineTransformMakeScale(1.3, 1.3);
+                          }completion:^(BOOL finished) {
+                              [UIView animateWithDuration:2 delay:0.0f options:UIViewAnimationOptionRepeat
+                                          animations:^{
+                             self.profileButton.transform = CGAffineTransformIdentity;
+                                          }
+                                               completion:^(BOOL finished) {
+                                                   
+                                               }];
+                     }];
+    
     [super viewDidLoad];
     [super viewDidLoad];
     FLAnimatedImage *image = [FLAnimatedImage animatedImageWithGIFData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://media.giphy.com/media/TdcmrLMF0TVqDLUjAT/giphy.gif"]]];
