973764f899d4658d9c041de33ddfa71c3fb9d617
Aaron Meheret <aaronm17@fb.com>
2019-08-07 17:23:51 -0700

    began implementing UI for cards view controller

--- HotSpot/CardCell.h
+++ HotSpot/CardCell.h
@@ -1,9 +1,8 @@
 //
 //  CardCell.h
-//  HotSpot
+//  
 //
 //  Created by aaronm17 on 8/7/19.
-//  Copyright © 2019 aodemuyi. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- HotSpot/CardCell.m
+++ HotSpot/CardCell.m
@@ -1,14 +1,14 @@
 //
 //  CardCell.m
-//  HotSpot
+//  
 //
 //  Created by aaronm17 on 8/7/19.
-//  Copyright © 2019 aodemuyi. All rights reserved.
 //
 
 #import "CardCell.h"
 
 @implementation CardCell
+
 
 - (void)awakeFromNib {
     [super awakeFromNib];
