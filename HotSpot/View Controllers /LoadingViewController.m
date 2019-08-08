1a53e5d4044f2bbfe477a623b1ebe7c0523010b3
Ayooluwa Odemuyiwa <aodemuyi@fb.com>
2019-08-08 16:19:21 -0700

    progress bar works

--- HotSpot/View Controllers /LoadingViewController.m
+++ HotSpot/View Controllers /LoadingViewController.m
@@ -18,13 +18,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    FLAnimatedImage *image = [FLAnimatedImage animatedImageWithGIFData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif"]]];
+    FLAnimatedImage *image = [FLAnimatedImage animatedImageWithGIFData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://media.giphy.com/media/TdcmrLMF0TVqDLUjAT/giphy.gif"]]];
     FLAnimatedImageView *imageView = [[FLAnimatedImageView alloc] init];
     imageView.animatedImage = image;
-    imageView.frame = CGRectMake(0.0, 0.0, 100.0, 100.0);
+    imageView.frame = CGRectMake(50.0, 50.0, 150.0, 150.0);
     [self.loadingGif addSubview:imageView];
     
 }
-
-
+/*
+- (void) fadeIn{
+        [_label setAlpha:0.0f];
+        //fade in
+        [UIView animateWithDuration:2.0f animations:^{
+            
+            [_label setAlpha:1.0f];
+            
+        } completion:^(BOOL finished) {
+            
+            //fade out
+            [UIView animateWithDuration:2.0f animations:^{
+                
+                [_label setAlpha:0.0f];
+                
+            } completion:nil];
+            
+        }];
+    }
+}
+*/ 
 @end
