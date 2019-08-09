a6bd0bbcb7754b642ce391142178c503e3a00aeb
Ayooluwa Odemuyiwa <aodemuyi@fb.com>
2019-08-08 21:02:20 -0700

    fade in fade out for animation

--- HotSpot/View Controllers /bookingLoading.m
+++ HotSpot/View Controllers /bookingLoading.m
@@ -10,8 +10,9 @@
 
 @interface bookingLoading ()
 // I realize now that this would've been easier programatically
-
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
 @property (strong, nonatomic) UIDynamicAnimator *animator;
+@property (weak, nonatomic) IBOutlet UILabel *bookingText;
 
 
 
@@ -21,13 +22,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.bookingText setAlpha:0.0f];
+    [UIView animateWithDuration:1.5f delay:0 options:UIViewAnimationOptionAutoreverse | UIViewAnimationOptionRepeat   animations:^{
+        [self.bookingText setAlpha:1.0f];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:1.5f delay:0 options:UIViewAnimationOptionAutoreverse | UIViewAnimationOptionRepeat animations:^{
+            [self.bookingText setAlpha:0.0f];
+        } completion:nil];
+    }];
+    
+    self.progressView.progress = 0;
     for (int i =0; i<=50; i++){
     UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"car3"]];
         imageView.tintColor = [UIColor colorWithDisplayP3Red:0.89406615499999997
                                                        green:0.3239448667
                                                         blue:0.2989487052
                                                        alpha:1.0];
-        imageView.frame = CGRectMake(i*14, i*20, 35,35);
+        imageView.frame = CGRectMake(i, i, 35,35);
     [self.view addSubview:imageView];
     [self animateBounce];
     }
@@ -46,7 +58,7 @@
 
 - (void) animateBounce{
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-    for (int i =0; i<=self.view.subviews.count-2; i++){
+    for (int i =0; i<=self.view.subviews.count-1; i++){
         if ([self.view.subviews[i] isKindOfClass:[UIImageView class]])
         {
         UIGravityBehavior* gravityBehavior =
@@ -60,10 +72,14 @@
         
         UIDynamicItemBehavior *elasticityBehavior =
         [[UIDynamicItemBehavior alloc] initWithItems:@[self.view.subviews[i]]];
-        elasticityBehavior.elasticity = 1;
+        elasticityBehavior.elasticity = .9;
         [self.animator addBehavior:elasticityBehavior];
     }
              }
+}
+
+- (void) timerFireMethod:(NSTimer*)timer{
+    self.progressView.progress+=.2;
 }
 
 @end
